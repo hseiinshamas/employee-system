15fb5adca169d5c1d4ebf29c7874db99753c1b60
Hussein Shamas <hssenshamas@gmail.com>
2023-09-08 00:05:00 +0300

    Added the frontend and the backend setup

---
+++ employee-system-ui/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import AddNewEmployee from './components/AddNewEmployee';
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <AddNewEmployee/>
+   </>
+  );
+}
+
+export default App;
